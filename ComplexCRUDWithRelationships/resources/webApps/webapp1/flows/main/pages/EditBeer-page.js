695b736306756302c6c405514a3efb7a885a4fae
David Konecny <david.konecny@oracle.com>
2018-02-16 13:48:52 +1300

    fixes few things in the ComplexCRUDWithRelationships/Beer app sample: * updating to Flow layout * removing ES6 syntax * fixing all Audit warnings * fixing missing OJET component imports * fixing missing limit and order query params on getCountry call in Create page

--- ComplexCRUDWithRelationships/resources/webApps/webapp1/flows/main/pages/EditBeer-page.js
+++ ComplexCRUDWithRelationships/resources/webApps/webapp1/flows/main/pages/EditBeer-page.js
@@ -6,7 +6,7 @@ define([], function () {
     // workaround for: BUFP-17180
     PageModule.prototype.assignStoredBeerQualities = function (helper, targetDefaultValue) {
         var source = helper.get("$chain.results.loadBeer.body.beerQualityCollection.items");
-        return source.map(q => q.quality);
+        return source.map(function(q) {return q.quality;});
     };
 
     PageModule.prototype.createCreatePart2 = function (beerQuality) {
@@ -20,12 +20,12 @@ define([], function () {
         var self = this;
         var changes = {};
         var qualitiesToDelete = beerRecord.beerQualityCollection.items.
-                filter(q => selectedQualities.indexOf(q.quality) === -1).
-                map(q => q.id);
+                filter(function(q) {return selectedQualities.indexOf(q.quality) === -1;}).
+                map(function(q) {return q.id;});
 
-        var originalQualities = beerRecord.beerQualityCollection.items.map(q => q.quality);
+        var originalQualities = beerRecord.beerQualityCollection.items.map(function(q) {return q.quality;});
         var qualitiesToAdd = selectedQualities.
-                filter(q => originalQualities.indexOf(q) === -1);
+                filter(function(q) {return originalQualities.indexOf(q) === -1;});
 
         var beerToSave = {
             name: beerRecord.name,
@@ -36,7 +36,7 @@ define([], function () {
         };
         changes.beerToSave = beerToSave;
         changes.recordsToDelete = qualitiesToDelete;
-        changes.recordsToCreate = qualitiesToAdd.map(q => self.createCreatePart2(q, beerRecord.id));
+        changes.recordsToCreate = qualitiesToAdd.map(function(q) {return self.createCreatePart2(q, beerRecord.id);});
         return changes;
     };
 
