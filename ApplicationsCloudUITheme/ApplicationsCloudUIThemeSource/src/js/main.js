2d0fda089f452c0fa8c7e2b156d1f7e520569363
Duncan Mills <duncan.mills@oracle.com>
2020-08-19 15:13:29 +0100

    Upgrade ApplicationsCloudUi Theme to JET 9.0.0 ready for Visual Builder 20.10 use

--- ApplicationsCloudUITheme/ApplicationsCloudUIThemeSource/src/js/main.js
+++ ApplicationsCloudUITheme/ApplicationsCloudUIThemeSource/src/js/main.js
@@ -27,22 +27,7 @@
       paths:
       // injector:mainReleasePaths
       {
-        'knockout': 'libs/knockout/knockout-3.5.0.debug',
-        'jquery': 'libs/jquery/jquery-3.4.1',
-        'jqueryui-amd': 'libs/jquery/jqueryui-amd-1.12.1',
-        'hammerjs': 'libs/hammer/hammer-2.0.8',
-        'ojdnd': 'libs/dnd-polyfill/dnd-polyfill-1.0.1',
-        'ojs': 'libs/oj/v8.1.0/debug' + (_ojNeedsES5 ? '_es5' : ''),
-        'ojL10n': 'libs/oj/v8.1.0/ojL10n',
-        'ojtranslations': 'libs/oj/v8.1.0/resources',
-        'text': 'libs/require/text',
-        'signals': 'libs/js-signals/signals',
-        'customElements': 'libs/webcomponents/custom-elements.min',
-        'proj4': 'libs/proj4js/dist/proj4-src',
-        'css': 'libs/require-css/css',
-        'touchr': 'libs/touchr/touchr',
-        'corejs': 'libs/corejs/shim',
-        'regenerator-runtime': 'libs/regenerator-runtime/runtime'
+
       }
       // endinjector
     }
