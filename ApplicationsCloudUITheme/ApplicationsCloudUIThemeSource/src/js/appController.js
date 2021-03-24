27c95bac38a02d59d27dfe8ceacdac895830c721
Duncan Mills <duncan.mills@oracle.com>
2021-03-24 09:34:08 +0000

    Add extra testing components to the theme home page to verify basics

--- ApplicationsCloudUITheme/ApplicationsCloudUIThemeSource/src/js/appController.js
+++ ApplicationsCloudUITheme/ApplicationsCloudUIThemeSource/src/js/appController.js
@@ -6,10 +6,10 @@
 /*
  * Your application specific code will go here
  */
-define(['ojs/ojcore', 'knockout', 'ojs/ojknockout','ojs/ojnavigationlist'],
-  function(oj, ko) {
-     function ControllerViewModel() {
-       var self = this;
+define(['ojs/ojcore', 'knockout', "ojs/ojarraydataprovider", 'ojs/ojknockout',  'ojs/ojnavigationlist', "ojs/ojformlayout", "ojs/ojslider", "ojs/ojinputnumber", "ojs/ojswitch"],
+  function (oj, ko, ArrayDataProvider) {
+    function ControllerViewModel() {
+      var self = this;
 
       // Media queries for repsonsive layouts
       var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
@@ -18,6 +18,17 @@ define(['ojs/ojcore', 'knockout', 'ojs/ojknockout','ojs/ojnavigationlist'],
       // Header
       // Application Name used in Branding Area
       self.appName = ko.observable("Applications Cloud UI Theme");
+
+      //For UI Tests
+      self.selectedNav = ko.observable("one");
+
+      var navdata = [
+        { name: "Selected", id: "one" },
+        { name: "unselected", id: "two" },
+        { name: "Disabled", disabled: "true", id: "three" }
+      ];
+      self.navDataDP = new ArrayDataProvider(navdata, { keyAttributes: "id" })
+
 
       // Footer
       function footerLink(name, id, linkTarget) {
@@ -32,8 +43,8 @@ define(['ojs/ojcore', 'knockout', 'ojs/ojknockout','ojs/ojnavigationlist'],
         new footerLink('Terms Of Use', 'termsOfUse', 'http://www.oracle.com/us/legal/terms/index.html'),
         new footerLink('Your Privacy Rights', 'yourPrivacyRights', 'http://www.oracle.com/us/legal/privacy/index.html')
       ]);
-     }
+    }
 
-     return new ControllerViewModel();
+    return new ControllerViewModel();
   }
 );
